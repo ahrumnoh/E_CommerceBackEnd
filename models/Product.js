22ebd601d6fc07014bdcdf6fdd69675a9329278f
ahrum noh <sydneyroseenglish@gmail.com>
2021-08-27 09:29:39 +1000

    removed

--- models/Product.js
+++ models/Product.js
@@ -9,45 +9,7 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNullL: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-
-    product_name: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-
-    price: {
-      type:DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
-    },
-
-    
-
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
+  
 );
 
 module.exports = Product;
--- models/Tag.js
+++ models/Tag.js
@@ -5,26 +5,6 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
-    id:{
-      type: DataTypes.INTEGER,
-      allowNullL: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
 
-    tag_name:{  
-      type: DataTypes.STRING,
-    }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
-);
 
 module.exports = Tag;
